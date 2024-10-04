94195075cc62e3cb4b4d36d2bfe67d88c04b0856
Bogdan-Daniel Petrutescu <petrutescubogdan@yahoo.com>
2024-10-04 07:06:44 +0000

    Create a view to handle the booking

--- restaurantbooking/bookings/views.py
+++ restaurantbooking/bookings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import bookingForm
 
 # Create your views here.
+
+
+def book_table(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = BookingForm()
+    return render(request, 'bookings/book_table.html', {'form': form})
+
+def success(request):
+    return render(request, 'bookings/success.html')
